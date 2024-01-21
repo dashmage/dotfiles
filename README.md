cf107d2c89deb8e40b737b9ea56717acb7268a7e
Ashley James <ashleyjames2800@gmail.com>
2024-01-21 17:35:47 +0530

    Add dotfiles for envy

---
+++ README.md
@@ -0,0 +1,3 @@
+My dotfiles managed with [chezmoi](https://www.chezmoi.io).
+
+Read the quick-start guide [here](https://www.chezmoi.io/quick-start/).
---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,49 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
+
+fish_add_path ~/.local/bin/
+
+alias nv=nvim
+alias bat=batcat
+alias fd=fdfind
+alias t='tmux new -A -s dashmage'
+alias cz=chezmoi
+
+# chezmoi (https://www.chezmoi.io/quick-start/)
+alias cz=chezmoi
+alias czd=chezmoi-cd
+alias cze='chezmoi edit --apply'
+
+alias j='juju'
+alias js='juju status'
+alias jsw='juju status --watch 5s'
+alias os='openstack'
+
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+
+# Moving up dirs easier
+alias ..='cd ..'
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# git aliases
+alias gs='git status -sb' # short status + show branch
+alias gd='git diff'
+alias gpl='git pull'
+alias gps='git push'
+alias gpf='git push -f' # force push
+alias gc='git commit -ev'
+alias ga='git add --all'
+alias gt='git log --graph --oneline --decorate' # pretty git tree log
+alias gcan='git commit --amend --no-edit'
+alias gco='git checkout'
+alias gr='git remote -v'
+
+set EDITOR "hx"
