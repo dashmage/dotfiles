f0d2b4cb17b533a4878680fdc1c3fb3153e009d8
Ashley James <ashleyjames2800@gmail.com>
2024-02-25 10:28:07 +0530

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,7 +10,7 @@ alias nv=nvim
 alias bat=batcat
 alias fd=fdfind
 alias t='tmux new -A -s dashmage'
-alias cz=chezmoi
+alias ze=zellij
 
 # chezmoi (https://www.chezmoi.io/quick-start/)
 alias cz=chezmoi
