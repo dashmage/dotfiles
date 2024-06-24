fcfae2361cb9686cb6b09b650f186c17f9a4bb54
Ashley James <ashley@dashmage.dev>
2024-06-24 17:19:50 +0530

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-    source ~/canonical/novarc_serverstack
     set ZELLIJ_AUTO_ATTACH true
     set ZELLIJ_AUTO_EXIT true
     eval (zellij setup --generate-auto-start fish | string collect)
