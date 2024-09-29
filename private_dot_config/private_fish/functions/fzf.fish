cc33c6f238f339171729d3db9fcb1e7ba2a8b5d3
Ashley James <ashley@dashmage.dev>
2024-09-29 13:03:03 +0530

    Update .config/fish/functions/fzf.fish

--- private_dot_config/private_fish/functions/fzf.fish
+++ private_dot_config/private_fish/functions/fzf.fish
@@ -1,10 +1,20 @@
+# directly open file in $EDITOR
 function fzf_open_in_editor
     set selected_file (fd --type f --hidden --follow --exclude .git | fzf --height 50% --preview "bat --color=always {}" --preview-window right,50%)
     if test -n "$selected_file"
         $EDITOR "$selected_file"
     end
-    commandline -f repaint # promt would get wonky otherwise
+    commandline -f repaint
+end
+
+# just print file path
+function fzf_search_in_directory
+    set selected_file (fd --type f --hidden --follow --exclude .git | fzf --height 50% --preview "bat --color=always {}" --preview-window right,50%)
+    if test -n "$selected_file"
+        commandline --current-token --replace "$selected_file"
+    end
+    commandline -f repaint
 end
 
 # bind ctrl-f
-bind \cf fzf_open_in_editor
+bind \cf fzf_search_in_directory
